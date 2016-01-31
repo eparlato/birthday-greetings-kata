4538d1519f34a9b5c881bcd16cd5f777d86b0c4b
eparlato <eparlato82@gmail.com>
2016-01-31 19:18:59 +0100

    We create greetings for an employee taken from the repository.

---
+++ src/test/java/it/eparlato/birthdaygreetings/CreateGreetingsTest.java
@@ -0,0 +1,77 @@
+package it.eparlato.birthdaygreetings;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CreateGreetingsTest {
+
+	@Rule
+	public final JUnitRuleMockery context = new JUnitRuleMockery();
+	
+	@Test
+	public void oneEmployeeCreateGreetings() throws Exception {
+		
+		final EmployeeRepository employeeRepository = context.mock(EmployeeRepository.class);
+		
+		final List<Employee> employees = new ArrayList<Employee>();
+		employees.add(new Employee());
+		
+		context.checking(new Expectations() {
+			{
+				allowing(employeeRepository).getEmployees();
+				will(returnValue(employees));
+			}
+		});
+		
+		GreetingsController greetingsController  = new GreetingsController(employeeRepository);	
+		greetingsController.process();
+		
+		assertEquals(1, greetingsController.getGreetings().size());
+	}
+	
+
+	public static class GreetingsController {
+
+		private EmployeeRepository employeeRepository;
+		private List<Greetings> greetingsList = new ArrayList<Greetings>();
+		
+		public GreetingsController(EmployeeRepository employeeRepository) {
+			this.employeeRepository = employeeRepository;
+		}
+
+		public List<Greetings> getGreetings() {
+			return greetingsList;
+		}
+
+		public void process() {
+			Employee employee = employeeRepository.getEmployees().get(0);
+			greetingsList.add(new Greetings(employee));
+		}
+	}
+
+	
+	public interface EmployeeRepository {
+
+		public List<Employee> getEmployees();
+
+	}
+	 
+	public static class Employee {
+
+	}
+	
+	public static class Greetings {
+
+		public Greetings(Employee employee) {
+		}
+
+	}
+
+}
