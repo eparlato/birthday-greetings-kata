ee40d0b3f838b9b20df84b229bccc289dc4e26c6
eparlato <eparlato82@gmail.com>
2016-02-06 18:15:23 +0100

    The repository will retunr only employees whose birthday is today. For every employee returned grettings will be sent. The case of oneEmployeeDoNotSendGreetings() is never going to happen then: test deleted.

--- src/test/java/it/eparlato/birthdaygreetings/CreateGreetingsTest.java
+++ src/test/java/it/eparlato/birthdaygreetings/CreateGreetingsTest.java
@@ -28,10 +28,11 @@ public class CreateGreetingsTest {
 		final List<Employee> employees = new ArrayList<Employee>();
 		employees.add(employee);
 		
+		final Date today = new SimpleDateFormat("dd/MM/yyyy").parse("03/02/2016");
 		
 		context.checking(new Expectations() {
 			{
-				allowing(employeeRepository).getEmployees();
+				allowing(employeeRepository).getEmployeesWhoseBirthadyIs(with(today));
 				will(returnValue(employees));
 				
 				oneOf(messageService).sendGreetingsToEmployee(employee);
@@ -39,31 +40,7 @@ public class CreateGreetingsTest {
 		});
 		
 		GreetingsController greetingsController  = new GreetingsController(employeeRepository, messageService);	
-		greetingsController.process(new SimpleDateFormat("dd/MM/yyyy").parse("03/02/2016"));
-	}
-	
-	@Test
-	public void oneEmployeeDoNotSendGreetings() throws Exception {
-		Date employeeDateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse("03/02/1982");
-		
-		final Employee employee = new Employee(employeeDateOfBirth);
-		final EmployeeRepository employeeRepository = context.mock(EmployeeRepository.class);	
-		final MessageService messageService = context.mock(MessageService.class);
-		final List<Employee> employees = new ArrayList<Employee>();
-		employees.add(employee);
-		
-		
-		context.checking(new Expectations() {
-			{
-				allowing(employeeRepository).getEmployees();
-				will(returnValue(employees));
-				
-				never(messageService).sendGreetingsToEmployee(employee);
-			}
-		});
-		
-		GreetingsController greetingsController  = new GreetingsController(employeeRepository, messageService);	
-		greetingsController.process(new SimpleDateFormat("dd/MM/yyyy").parse("17/11/2016"));
+		greetingsController.process(today);
 	}
 	
 	public class GreetingsController {
@@ -77,7 +54,7 @@ public class CreateGreetingsTest {
 		}
 
 		public void process(Date today) {
-			Employee employee = employeeRepository.getEmployees().get(0);
+			Employee employee = employeeRepository.getEmployeesWhoseBirthadyIs(today).get(0);
 			
 			if(employee.isBirthday(today)) {
 				messageService.sendGreetingsToEmployee(employee);
@@ -87,15 +64,11 @@ public class CreateGreetingsTest {
 
 	
 	public interface EmployeeRepository {
-
-		public List<Employee> getEmployees();
-
+		public List<Employee> getEmployeesWhoseBirthadyIs(Date today);
 	}
 	
 	public interface MessageService {
-
 		void sendGreetingsToEmployee(Employee employee);
-
 	}
 
 }
