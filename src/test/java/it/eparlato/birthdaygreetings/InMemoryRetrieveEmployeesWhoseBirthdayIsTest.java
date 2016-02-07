baed19ec9d80d4bcef8492d7d0f3b92dc3e990a0
eparlato <eparlato82@gmail.com>
2016-02-07 22:04:26 +0100

    Since InMemoryEmployeeRepository it's empty, it doesn't return any employee whose birthday is today.

---
+++ src/test/java/it/eparlato/birthdaygreetings/InMemoryRetrieveEmployeesWhoseBirthdayIsTest.java
@@ -0,0 +1,34 @@
+package it.eparlato.birthdaygreetings;
+
+import static org.junit.Assert.assertTrue;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+public class InMemoryRetrieveEmployeesWhoseBirthdayIsTest {
+
+
+	@Test
+	public void noEmployeesWhoseBirthdayIsToday() throws Exception {
+		
+		InMemoryEmployeeRepository inMemoryEmployeeRepository = new InMemoryEmployeeRepository();
+		
+		Date today = new SimpleDateFormat("dd/MM/yyyy").parse("31/12/2015");
+		
+		List<Employee> employees = inMemoryEmployeeRepository.getEmployeesWhoseBirthadyIs(today);
+		
+		assertTrue(employees.isEmpty());
+	}
+	
+	public class InMemoryEmployeeRepository {
+
+		public List<Employee> getEmployeesWhoseBirthadyIs(Date today) {
+			return new ArrayList<Employee>();
+		}
+		
+	}
+}
