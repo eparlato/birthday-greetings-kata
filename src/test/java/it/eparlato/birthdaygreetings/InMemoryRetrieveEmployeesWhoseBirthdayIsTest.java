28bb3984790864be139dfdcad647e0e7609a5a31
eparlato <eparlato82@gmail.com>
2016-02-07 23:18:09 +0100

    If the in-memory repository contains a single Employee whose birthday is today, return a list with it.

--- src/test/java/it/eparlato/birthdaygreetings/InMemoryRetrieveEmployeesWhoseBirthdayIsTest.java
+++ src/test/java/it/eparlato/birthdaygreetings/InMemoryRetrieveEmployeesWhoseBirthdayIsTest.java
@@ -1,34 +1,64 @@
 package it.eparlato.birthdaygreetings;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class InMemoryRetrieveEmployeesWhoseBirthdayIsTest {
 
-
 	@Test
 	public void noEmployeesWhoseBirthdayIsToday() throws Exception {
-		
-		InMemoryEmployeeRepository inMemoryEmployeeRepository = new InMemoryEmployeeRepository();
-		
+
+		InMemoryEmployeeRepository inMemoryEmployeeRepository = new InMemoryEmployeeRepository(
+				Collections.<Employee> emptyList());
+
 		Date today = new SimpleDateFormat("dd/MM/yyyy").parse("31/12/2015");
-		
+
 		List<Employee> employees = inMemoryEmployeeRepository.getEmployeesWhoseBirthadyIs(today);
-		
+
 		assertTrue(employees.isEmpty());
 	}
-	
+
+	@Test
+	public void oneEmployeeWhoseBirthdayIsToday() throws Exception {
+		Employee employee = new Employee(new SimpleDateFormat("dd/MM/yyyy").parse("31/12/1976"));
+
+		InMemoryEmployeeRepository inMemoryEmployeeRepository = new InMemoryEmployeeRepository(
+				Collections.singletonList(employee));
+
+		Date today = new SimpleDateFormat("dd/MM/yyyy").parse("31/12/2015");
+
+		List<Employee> employees = inMemoryEmployeeRepository.getEmployeesWhoseBirthadyIs(today);
+
+		assertEquals(employee, employees.get(0));
+	}
+
 	public class InMemoryEmployeeRepository implements EmployeeRepository {
 
-		public List<Employee> getEmployeesWhoseBirthadyIs(Date today) {
-			return new ArrayList<Employee>();
+		private List<Employee> employees;
+
+		public InMemoryEmployeeRepository(List<Employee> employees) {
+			this.employees = employees;
 		}
-		
+
+		public List<Employee> getEmployeesWhoseBirthadyIs(Date today) {
+			List<Employee> employeesWhoseBirthdayIsToday = new ArrayList<Employee>();
+			for (Employee employee : employees) {
+				if (employee.isBirthday(today)) {
+					employeesWhoseBirthdayIsToday.add(employee);
+				}
+			}
+
+			return employeesWhoseBirthdayIsToday;
+		}
+
 	}
 }
